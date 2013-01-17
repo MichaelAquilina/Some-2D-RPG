f3eaca0d11858e736cb797fad1dc1ba76f408c58
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-17 21:06:09 +0100

    Added Generators Interface and RandomWorldGenerator class

---
+++ DivineRightConcept/DivineRightConcept/Generators/RandomWorldGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DivineRightConcept.Generators
+{
+    public class RandomWorldGenerator : IWorldGenerator
+    {
+        public int[][] Generate(int Width, int Height)
+        {
+            int[][] result = new int[Width][];
+            Random randomGen = new Random();
+
+            for (int i = 0; i < Width; i++)
+                result[i] = new int[Height];
+
+            //Generate Base
+            for (int i = 0; i < Width; i++)
+                for (int j = 0; j < Height; j++)
+                    result[i][j] = Ground.GROUND_TEXTURE_GRASS;
+
+            //Add Random Patches of Thundra
+            int thundraCount = randomGen.Next(5, 8);
+            for (int i = 0; i < thundraCount; i++)
+            {
+                int x = randomGen.Next(0, Width - 1);
+                int y = randomGen.Next(0, Height - 1);
+
+                result[x][y] = Ground.GROUND_TEXTURE_THUNDRA;
+                result[x + 1][y + 1] = Ground.GROUND_TEXTURE_THUNDRA;
+            }
+            
+            return result;
+        }
+    }
+}
