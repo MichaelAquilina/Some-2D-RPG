abe6ceb1c74e312964705485dbde5faac4d3466e
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-17 23:07:51 +0100

    VERY simple demonstration on how Actors can update using AI

---
+++ DivineRightConcept/DivineRightConcept/GameObjects/SimpleAIActor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace DivineRightConcept.GameObjects
+{
+    //a VERY simple demonstration how Actors can exhibit forms of AI
+    public class SimpleAIActor : Actor
+    {
+        double prevGameTime = 0;
+
+        public SimpleAIActor(int X, int Y)
+            :base(X,Y)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (gameTime.TotalGameTime.TotalMilliseconds - prevGameTime > 800)
+            {
+                prevGameTime = gameTime.TotalGameTime.TotalMilliseconds;
+                X += 1;
+            }
+        }
+    }
+}
