2a68e497d0a779f9d89a11d046c7aed2441586f9
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-17 19:46:04 +0100

    Added Delay between User Actions on the keyboard

--- DivineRightConcept/DivineRightConcept/DivineRightGame.cs
+++ DivineRightConcept/DivineRightConcept/DivineRightGame.cs
@@ -15,27 +15,25 @@ namespace DivineRightConcept
 {
     public class DivineRightGame : Microsoft.Xna.Framework.Game
     {
+        //Constant (Editable) Valuables
         const bool DEBUG = true;
+        const int WORLD_HEIGHT = 200;
+        const int WORLD_WIDTH = 200;
+        const int TILE_WIDTH = 15;
+        const int TILE_HEIGHT = 15;
+        const int VIEW_WIDTH = 450;
+        const int VIEW_HEIGHT = 450;
 
+        //Graphic Related Variables
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
-        GameTime prevGameTime = new GameTime();
-        int x = 5;
-        int y = 5;
-        Texture2D _stickManTexture;
-
         SpriteFont _defaultSpriteFont;
 
-        //all this needs to move to a seperate class!
-        int WORLD_HEIGHT = 200;
-        int WORLD_WIDTH = 200;
-        int TILE_WIDTH = 15;
-        int TILE_HEIGHT = 15;
-        int VIEW_WIDTH = 500;
-        int VIEW_HEIGHT = 500;
-
+        //Game Specific Variablies
+        double prevGameTime = 0;
         GameWorld _worldComponent;
+        int PlayerX = 0;
+        int PlayerY = 0;
 
         public DivineRightGame()
         {
@@ -46,7 +44,7 @@ namespace DivineRightConcept
 
         protected override void Initialize()
         {
-            _worldComponent = new GameWorld(this, WORLD_WIDTH, WORLD_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            _worldComponent = new GameWorld(this, WORLD_WIDTH, WORLD_HEIGHT);
             _worldComponent.Initialize();
 
             base.Initialize();
@@ -74,32 +72,35 @@ namespace DivineRightConcept
 
             KeyboardState keyboardState = Keyboard.GetState();
 
+            if (gameTime.TotalGameTime.TotalMilliseconds - prevGameTime > 50)
+            {
                 if (keyboardState.IsKeyDown(Keys.Up))
                 {
-                    prevGameTime = gameTime;
-                    y = y - 1;
+                    prevGameTime = gameTime.TotalGameTime.TotalMilliseconds;
+                    PlayerY = PlayerY - 1;
                 }
                 if (keyboardState.IsKeyDown(Keys.Down))
                 {
-                    prevGameTime = gameTime;
-                    y = y + 1;
+                    prevGameTime = gameTime.TotalGameTime.TotalMilliseconds;
+                    PlayerY = PlayerY + 1;
                 }
                 if (keyboardState.IsKeyDown(Keys.Left))
                 {
-                    prevGameTime = gameTime;
-                    x = x - 1;
+                    prevGameTime = gameTime.TotalGameTime.TotalMilliseconds;
+                    PlayerX = PlayerX - 1;
                 }
                 if (keyboardState.IsKeyDown(Keys.Right))
                 {
-                    prevGameTime = gameTime;
-                    x = x + 1;
+                    prevGameTime = gameTime.TotalGameTime.TotalMilliseconds;
+                    PlayerX = PlayerX + 1;
                 }
 
                 //prevent from going out of range
-                if (x < 0) x = 0;
-                if (y < 0) y = 0;
-                if (x >= WORLD_WIDTH) x = WORLD_WIDTH - 1;
-                if (y >= WORLD_HEIGHT) y = WORLD_HEIGHT - 1;
+                if (PlayerX < 0) PlayerX = 0;
+                if (PlayerY < 0) PlayerY = 0;
+                if (PlayerX >= WORLD_WIDTH) PlayerX = WORLD_WIDTH - 1;
+                if (PlayerY >= WORLD_HEIGHT) PlayerY = WORLD_HEIGHT - 1;
+            }
 
             base.Update(gameTime);
         }
@@ -110,10 +111,10 @@ namespace DivineRightConcept
 
             spriteBatch.Begin();
 
-            _worldComponent.DrawWorldViewPort(spriteBatch, x, y, new Rectangle(100, 0, VIEW_WIDTH, VIEW_HEIGHT));
+            _worldComponent.DrawWorldViewPort(spriteBatch, PlayerX, PlayerY, TILE_WIDTH, TILE_HEIGHT, new Rectangle(100, 0, VIEW_WIDTH, VIEW_HEIGHT));
 
             //DRAW DEBUGGING INFORMATION
-            spriteBatch.DrawString(_defaultSpriteFont, x + "," + y, new Vector2(0, 0), Color.White);
+            spriteBatch.DrawString(_defaultSpriteFont, PlayerX + "," + PlayerY, new Vector2(0, 0), Color.White);
 
             spriteBatch.End();
 
