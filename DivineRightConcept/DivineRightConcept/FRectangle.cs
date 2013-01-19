59424c79d70cfab2ed32fe27f62e782f0a09c2b6
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-19 18:44:15 +0100

    Added Float Rectangle

---
+++ DivineRightConcept/DivineRightConcept/FRectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DivineRightConcept
+{
+    //A Rectangle structure capable of holding float values
+    public class FRectangle
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public FRectangle(float X, float Y, float Width, float Height)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Width = Width;
+            this.Height = Height;
+        }
+    }
+}
