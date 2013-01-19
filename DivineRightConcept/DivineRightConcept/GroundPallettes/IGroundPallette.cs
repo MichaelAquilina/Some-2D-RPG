be23f41056d10dd283b66c596b3b6cba34b38737
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-19 14:23:45 +0100

    Added Support for GroundPallettes

---
+++ DivineRightConcept/DivineRightConcept/GroundPallettes/IGroundPallette.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DivineRightConcept.GroundPallettes
+{
+    //Ground Pallette interface that specifies the required properties and methods that would allow
+    //for the Game to proporly be able to render the pallette.
+    public interface IGroundPallette
+    {
+        void LoadContent(ContentManager Content);
+
+        void DrawGroundTexture(SpriteBatch SpriteBatch, int TileType, Rectangle DesRectangle);
+
+        Color GetTileColor(int TileType);
+
+        int TileCount { get; }
+    }
+}
