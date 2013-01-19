cdf7569c4ee458b0a5213951352ca3445c8360e3
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-19 14:24:22 +0100

    Renamed Generators folder to WorldGenerators

---
+++ DivineRightConcept/DivineRightConcept/WorldGenerators/IWorldGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DivineRightConcept.WorldGenerators
+{
+    public interface IWorldGenerator
+    {
+        Map Generate(int Width, int Height);
+    }
+}
