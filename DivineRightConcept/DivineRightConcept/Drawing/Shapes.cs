016439847bed4ebb89caea672104f7202d27eaca
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-20 15:42:57 +0100

    Added Drawing namespace for Drawing extension methods

---
+++ DivineRightConcept/DivineRightConcept/Drawing/Shapes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DivineRightConcept.Drawing
+{
+    /// <summary>
+    /// Class that provides static extension methods for drawing Geometrical shapes on the Display
+    /// </summary>
+    public static class Shapes
+    {
+        public static void DrawRectangle(this SpriteBatch SpriteBatch, Rectangle DestRectangle, Color Background)
+        {
+            Texture2D rectText = new Texture2D(SpriteBatch.GraphicsDevice, 1, 1);
+            rectText.SetData<Color>(new Color[] { Background });
+
+            SpriteBatch.Draw(rectText, DestRectangle, Color.White);
+        }
+    }
+}
