635caa48b864c811096e9daa5c9107ab6a3588a7
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-20 17:40:57 +0100

    Added Sprite Sheet Animation Support

---
+++ DivineRightConcept/DivineRightConcept/Drawing/Animation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DivineRightConcept.Drawing
+{
+    /// <summary>
+    /// Animation class that allows the user to specify an animation from a texture
+    /// </summary>
+    public class Animation
+    {
+        public Texture2D SpriteSheet { get; set; }
+        public string Name { get; set; }
+        public Rectangle[] Frames { get; set; }
+
+        public Animation(Texture2D SpriteSheet, string Name, Rectangle[] Frames)
+        {
+            this.SpriteSheet = SpriteSheet;
+            this.Name = Name;
+            this.Frames = Frames;
+        }
+
+        public Rectangle GetCurrentFrame(GameTime GameTime)
+        {
+            int index = (int)(GameTime.TotalGameTime.TotalMilliseconds / 100);
+            return Frames[index % Frames.Length];
+        }
+    }
+}
