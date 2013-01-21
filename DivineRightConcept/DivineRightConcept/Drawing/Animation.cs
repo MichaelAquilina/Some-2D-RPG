5e5163aa88253026eba705ede5112657c4572596
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-21 17:58:06 +0100

    Added support for Resetting the Animation

--- DivineRightConcept/DivineRightConcept/Drawing/Animation.cs
+++ DivineRightConcept/DivineRightConcept/Drawing/Animation.cs
@@ -10,13 +10,13 @@ namespace DivineRightConcept.Drawing
     /// <summary>
     /// Animation class that allows the user to specify an animation from a texture
     /// </summary>
-    //TODO: Specify start time. This way, the animation can be reset from its start point
-    //rather than looping based solely on the GameTime.
     public class Animation
     {
         public Texture2D SpriteSheet { get; set; }
         public Rectangle[] Frames { get; set; }
         public int FrameChange { get; set; }
+
+        private double _startTime = 0;
 
         public Animation(Texture2D SpriteSheet, Rectangle[] Frames, int FrameChange=100)
         {
@@ -25,9 +25,28 @@ namespace DivineRightConcept.Drawing
             this.FrameChange = FrameChange;
         }
 
+        /// <summary>
+        /// Resets the Animation to the first frame. Requires the GameTime as an input
+        /// parameters so that the animation may know at point in time the game is at.
+        /// This shouldnt be a problem since most of the logic involved with animations
+        /// will occur in Draw and Update methods - both of which are passed GameTime
+        /// parameters.
+        /// </summary>
+        /// <param name="GameTime">Current GameTime that the game is at.</param>
+        public void ResetAnimation(GameTime GameTime)
+        {
+            _startTime = GameTime.TotalGameTime.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// Returns the Current Frame to show in the sprite sheet based on the current
+        /// games running time.
+        /// </summary>
+        /// <param name="GameTime">GameTime object representing the current state in time of the game.</param>
+        /// <returns>Rectangle object representing the Frame in the spritesheet to show.</returns>
         public Rectangle GetCurrentFrame(GameTime GameTime)
         {
-            int index = (int)(GameTime.TotalGameTime.TotalMilliseconds / FrameChange);
+            int index = (int)((GameTime.TotalGameTime.TotalMilliseconds - _startTime) / FrameChange);
             return Frames[index % Frames.Length];
         }
     }
