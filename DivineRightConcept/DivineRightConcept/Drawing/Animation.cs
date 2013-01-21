dc1fac7874aff38b802f4ad50aa39b0ae3c7a73a
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-21 10:46:37 +0100

    comment about improving animation framework

--- DivineRightConcept/DivineRightConcept/Drawing/Animation.cs
+++ DivineRightConcept/DivineRightConcept/Drawing/Animation.cs
@@ -10,6 +10,8 @@ namespace DivineRightConcept.Drawing
     /// <summary>
     /// Animation class that allows the user to specify an animation from a texture
     /// </summary>
+    //TODO: Specify start time. This way, the animation can be reset from its start point
+    //rather than looping based solely on the GameTime.
     public class Animation
     {
         public Texture2D SpriteSheet { get; set; }
