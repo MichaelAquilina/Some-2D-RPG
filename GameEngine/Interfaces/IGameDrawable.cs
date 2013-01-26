7e27a73295ff09d7b5004878dbc2ac3c8921de66
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-26 17:30:08 +0100

    Added Comments

--- GameEngine/Interfaces/IGameDrawable.cs
+++ GameEngine/Interfaces/IGameDrawable.cs
@@ -14,11 +14,11 @@ namespace GameEngine.Interfaces
         float Width { get; }
         float Height { get; }
 
-        bool Visible { get; }
+        bool Visible { get; }           //Value Specifying if the Drawable Object should currently be visible
 
-        Color DrawColor { get; }
+        Color DrawColor { get; }        //Color mask with which to draw the SpriteBatch. Should be White by Default
 
-        Vector2 Origin { get; }
+        Vector2 Origin { get; }         //Relative Origin. 0,0 Being Top Left Corner and 1,1 Being Bottom Right Corner
 
         Texture2D GetTexture(GameTime GameTime);
         Rectangle GetSourceRectangle(GameTime GameTime);
