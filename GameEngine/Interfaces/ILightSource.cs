bc33fd2fe59e2025dd7f6875cfedf902ca1b86a0
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-31 21:56:20 +0100

    Completed Interface hooks

--- GameEngine/Interfaces/ILightSource.cs
+++ GameEngine/Interfaces/ILightSource.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace GameEngine.Interfaces
 {
     public interface ILightSource
     {
+        Texture2D GetLightSourceTexture(GameTime gameTime);
+
+        Rectangle GetLightSourceRectangle(GameTime gameTime);
+
+        Rectangle GetReleativeDestRectangle(GameTime gameTime);
     }
 }
