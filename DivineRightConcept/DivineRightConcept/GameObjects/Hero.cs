263bb79d078118b6e66b5a531c5d56a4a42a356e
KillaW0lf04 <michaelaquilina@gmail.com>
2013-02-05 19:46:50 +0100

    Added Hero

---
+++ DivineRightConcept/DivineRightConcept/GameObjects/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameEngine.GameObjects;
+using GameEngine.Interfaces;
+using Microsoft.Xna.Framework.Content;
+
+namespace DivineRightConcept.GameObjects
+{
+    public class Hero : Actor, ILoadable
+    {
+        public Hero(float X, float Y) :
+            base(X, Y, 1.5f, 1.5f)
+        {
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.LoadAnimationXML(@"Animations/Knuckles.anim", Content);
+
+            SetCurrentAnimation("Idle");
+        }
+
+        public void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
