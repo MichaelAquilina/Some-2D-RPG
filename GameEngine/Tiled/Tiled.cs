0a3e6878218fc184150917a66df1183af01d75c2
KillaW0lf04 <michaelaquilina@gmail.com>
2013-02-24 22:36:48 +0100

    Added Tiled Classes

---
+++ GameEngine/Tiled/Tiled.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace GameEngine.Tiled
+{
+    public class Tiled
+    {
+        public static TiledMap LoadTiledXML(string file)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(file);
+
+            XmlNode mapNode = document.SelectSingleNode("map");
+
+            TiledMap map = new TiledMap();
+            map.Width = Convert.ToInt32(mapNode.Attributes["width"].Value);
+            map.Height = Convert.ToInt32(mapNode.Attributes["height"].Value);
+            map.TileWidth = Convert.ToInt32(mapNode.Attributes["tilewidth"].Value);
+            map.TileHeight = Convert.ToInt32(mapNode.Attributes["tileheight"].Value);
+
+            foreach (XmlNode tilesetNode in mapNode.SelectNodes("tileset"))
+            {
+                TileSet tileset = new TileSet();
+                tileset.FirstGID = Convert.ToInt32(tilesetNode.Attributes["firstgid"].Value);
+                tileset.Name = tilesetNode.Attributes["name"].Value;
+                tileset.TileHeight = Convert.ToInt32(tilesetNode.Attributes["tileheight"].Value);
+                tileset.TileWidth = Convert.ToInt32(tilesetNode.Attributes["tilewidth"].Value);
+
+                XmlNode imageNode = tilesetNode.SelectSingleNode("image");
+                tileset.Source = imageNode.Attributes["source"].Value;
+                tileset.ImageHeight = Convert.ToInt32(imageNode.Attributes["height"].Value);
+                tileset.ImageWidth = Convert.ToInt32(imageNode.Attributes["width"].Value);
+
+                map.TileSets.Add(tileset);
+            }
+
+            foreach (XmlNode layerNode in mapNode.SelectNodes("layer"))
+            {
+                int width = Convert.ToInt32(layerNode.Attributes["width"].Value);
+                int height = Convert.ToInt32(layerNode.Attributes["height"].Value);
+
+                TileLayer layer = new TileLayer(width, height);
+
+                XmlNode dataNode = layerNode.SelectSingleNode("data");
+                string[] line = dataNode.InnerText.Split('\n');
+
+                int x = 0;
+                int y = 0;
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    x = 0;
+
+                    //dummy lines should be skipped
+                    if (line[j].Length == 0)
+                        continue;
+
+                    string[] items = line[j].Split(',');
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        if (items[i].Length == 0)
+                            continue;
+
+                        layer._tiles[y][x] = Convert.ToInt32(items[i]);
+                        x = x + 1;
+                    }
+
+                    y = y + 1;
+                }    
+
+                map.TileLayers.Add(layer);
+            }
+
+            return map;
+        }
+    }
+}
---
+++ GameEngine/Tiled/TiledMap.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngine.Tiled
+{
+    public enum Orientation { orthoganal, isometric }
+
+    public class TileLayer
+    {
+        public string Name { get; set; }
+
+        public int Width {
+            get { return _tiles.Length; }
+        }
+        public int Height { 
+            get { return _tiles[0].Length; }
+        }
+
+        internal int[][] _tiles;
+
+        public TileLayer(int Width, int Height)
+        {
+            _tiles = new int[Height][];
+            for (int i = 0; i < Height; i++)
+                _tiles[i] = new int[Width];
+        }
+    }
+
+    public class TileSet
+    {
+        public string Name { get; set; }
+        public int TileWidth { get; set; }
+        public int TileHeight { get; set; }
+        public int FirstGID { get; set; }
+
+        public int ImageWidth { get; set; }
+        public int ImageHeight { get; set; }
+
+        public string Source { get; set; }
+    }
+
+    public class TiledMap
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int TileWidth { get; set; }
+        public int TileHeight { get; set; }
+
+        public List<TileSet> TileSets { get; set; }
+        public List<TileLayer> TileLayers { get; set; }
+
+        public TiledMap()
+        {
+            TileSets = new List<TileSet>();
+            TileLayers = new List<TileLayer>();
+        }
+    }
+}
