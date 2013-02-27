acf3b08a23a5e18a13785d3ea160802450d1f7d9
KillaW0lf04 <michaelaquilina@gmail.com>
2013-02-27 20:18:00 +0100

    Added BasicLightSource for quick addition of light points on the map

---
+++ ShadowKillGame/ShadowKill/GameObjects/BasicLightSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using ShadowKill.Shaders;
+
+namespace ShadowKillGame.GameObjects
+{
+    public class BasicLightSource : ILightSource
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public float RadiusX { get; set; }
+        public float RadiusY { get; set; }
+
+        public PositionType PositionType { get; set; }
+
+        public Color LightColor { get; set; }
+
+        public BasicLightSource(float X, float Y, float RadiusX, float RadiusY, Color LightColor, PositionType PositionType=PositionType.Relative)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.RadiusX = RadiusX;
+            this.RadiusY = RadiusY;
+            this.LightColor = LightColor;
+            this.PositionType = PositionType;
+        }
+    }
+}
