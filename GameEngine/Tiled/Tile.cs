2c8c9d312e9e35d5a001edf0f23371ee82a8febe
KillaW0lf04 <michaelaquilina@gmail.com>
2013-02-27 21:26:17 +0100

    Added Tile Representation

---
+++ GameEngine/Tiled/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace GameEngine.Tiled
+{
+    public class Tile
+    {
+        public Texture2D SourceTexture { get; set; }
+        public Rectangle SourceRectangle { get; set; }
+        public int TileGid { get; set; }
+        public Dictionary<string, string> Properties { get; private set; }
+    }
+}
