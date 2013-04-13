ee68a1fc9fb4ccdfe4dffabeac1d9382ecc8aec3
KillaW0lf04 <michaelaquilina@gmail.com>
2013-04-13 14:48:28 +0200

    Added simple bat AI for following player when within a certain distance

--- Some2DRPG/Some2DRPG/GameObjects/Bat.cs
+++ Some2DRPG/Some2DRPG/GameObjects/Bat.cs
@@ -11,7 +11,10 @@ namespace Some2DRPG.GameObjects
     {
         private static Random randomGenerator = new Random();
 
+        private const double AGRO_DISTANCE = 200;
         private double _randomModifier;
+
+        private float _moveSpeed = 1.5f;
 
         public Bat(int PX, int PY)
             :base(PX, PY)
@@ -22,19 +25,44 @@ namespace Some2DRPG.GameObjects
 
         public override void Update(GameTime gameTime, TeeEngine engine)
         {
+            // Store previous position for latter calculations.
             Vector2 prevPos = Pos;
+            Hero player = (Hero) engine.GetEntity("Player");
 
-            Pos.X += (float) (Math.Cos(gameTime.TotalGameTime.TotalSeconds - _randomModifier*90) * 2);
+            double distance = Vector2.Distance(player.Pos, this.Pos);
 
-            // Determine the animation based on the change in position
-            if (prevPos.X < Pos.X)
-                this.CurrentDrawableState = "Right";
-            if (prevPos.X > Pos.X)
-                this.CurrentDrawableState = "Left";
-            if (prevPos.Y < Pos.Y)
-                this.CurrentDrawableState = "Down";
-            if (prevPos.Y > Pos.Y)
-                this.CurrentDrawableState = "Up";
+            if (distance < AGRO_DISTANCE)
+            {
+                // Move towards the player for an attack move.
+                double angle = Math.Atan2(
+                    player.Pos.Y - this.Pos.Y, 
+                    player.Pos.X - this.Pos.X
+                    );
+
+                Pos.X += (float) (Math.Cos(angle) * _moveSpeed);
+                Pos.Y += (float) (Math.Sin(angle) * _moveSpeed);
+            }
+            else
+            {
+                // Perform a standard patrol action.
+                Pos.X += (float)(Math.Cos(gameTime.TotalGameTime.TotalSeconds - _randomModifier * 90) * 2);
+            }
+
+            // Determine the animation based on the change in position.
+            if (Math.Abs(prevPos.X - Pos.X) > Math.Abs(prevPos.Y - Pos.Y))
+            {
+                if (prevPos.X < Pos.X)
+                    this.CurrentDrawableState = "Right";
+                if (prevPos.X > Pos.X)
+                    this.CurrentDrawableState = "Left";
+            }
+            else
+            {
+                if (prevPos.Y < Pos.Y)
+                    this.CurrentDrawableState = "Down";
+                if (prevPos.Y > Pos.Y)
+                    this.CurrentDrawableState = "Up";
+            }
         }
 
         public override void LoadContent(ContentManager content)
