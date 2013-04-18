9b004ae35cc7f830fae16ec7ce5a60b9dc24441d
KillaW0lf04 <michaelaquilina@gmail.com>
2013-04-18 13:20:23 +0200

    Added new GameOptions class

---
+++ GameEngine/Options/DrawingOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace GameEngine.Options
+{
+    public class DrawingOptions
+    {
+        /// <summary>
+        /// Show Entity debug information when drawing the world view port to the screen.
+        /// </summary>
+        public bool ShowEntityDebugInfo { get; set; }
+
+        /// <summary>
+        /// Shows the QuadTrees bounding boxes when drawing the world viewport.
+        /// </summary>
+        public bool ShowQuadTree { get; set; }
+
+        /// <summary>
+        /// bool value specifying if the tile grid should be shown during render calls.
+        /// </summary>
+        public bool ShowTileGrid { get; set; }
+
+        /// <summary>
+        /// bool value specifying if the bounding boxes for entities should be shown during render calls.
+        /// </summary>
+        public bool ShowBoundingBoxes { get; set; }
+
+        /// <summary>
+        /// bool value specifying if the bounding boxes for each individual drawable component should be shown.
+        /// </summary>
+        public bool ShowDrawableComponents { get; set; }
+    }
+}
