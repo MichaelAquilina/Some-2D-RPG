f4362055862a0bc4100e98485c9007f2d7ec4ed0
KillaW0lf04 <michaelaquilina@gmail.com>
2013-05-08 18:58:50 +0200

    Added PreDestory and PreCreate event handlers to StaticEntity. Currently do not support return values to specify create or destroy result.

--- GameEngine/GameObjects/StaticEntity.cs
+++ GameEngine/GameObjects/StaticEntity.cs
@@ -4,21 +4,40 @@ namespace GameEngine.GameObjects
 {
     /// <summary>
     /// Represents a Static object on the map that is not associated with any particular Entity
-    /// subclass. By wrapping the Entity in this StaticEntity class, the user may specify Update
-    /// routines to run within the tile editor through the use of event hooks.
+    /// subclass. By wrapping the Entity in this StaticEntity class, the user may specify Update,
+    /// PreCreate, PreDestroy, PostCreate and PostDestroy routines to run within the tile editor 
+    /// through the use of event hooks.
     /// </summary>
     public class StaticEntity : Entity
     {
         public delegate void EntityEventHandler(Entity caller, GameTime gameTime, TeeEngine engine);
 
         public event EntityEventHandler UpdateEvent;
-        public event EntityEventHandler PostDestroyEvent;
+        public event EntityEventHandler PreCreateEvent;
         public event EntityEventHandler PostCreateEvent;
+        public event EntityEventHandler PreDestroyEvent;
+        public event EntityEventHandler PostDestroyEvent;
+
+        public override bool PreCreate(GameTime gameTime, TeeEngine engine)
+        {
+            if (PreCreateEvent != null)
+                PreCreateEvent(this, gameTime, engine);
+
+            return true;
+        }
 
         public override void PostCreate(GameTime gameTime, TeeEngine engine)
         {
             if (PostCreateEvent != null)
                 PostCreateEvent(this, gameTime, engine);
+        }
+
+        public override bool PreDestroy(GameTime gameTime, TeeEngine engine)
+        {
+            if (PreDestroyEvent != null)
+                PreDestroyEvent(this, gameTime, engine);
+
+            return true;
         }
 
         public override void PostDestroy(GameTime gameTime, TeeEngine engine)
