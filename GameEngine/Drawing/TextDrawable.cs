9e8e9f64f326ce04de24a10062d410e85dda4349
KillaW0lf04 <michaelaquilina@gmail.com>
2013-05-10 18:00:24 +0200

    Added TextDrawable abstract class for rendering text to the screen.

---
+++ GameEngine/Drawing/TextDrawable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using GameEngine.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameEngine.Drawing
+{
+    public abstract class TextDrawable : IGameDrawable
+    {
+        public Vector2 Origin { get; set; }
+        
+        public TextDrawable()
+        {
+            Origin = Vector2.Zero;
+        }
+
+        public int GetWidth(double elapsedMS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHeight(double elapsedMS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual SpriteFont GetSpriteFont(double elapsedMS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string GetText(double elapsedMS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFinished(double elapsedMS)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Rectangle destRectangle, Color color, float rotation, Vector2 origin, SpriteEffects spriteEffects, float layerDepth, double elapsedMS)
+        {
+            float scale = destRectangle.Height / GetHeight(elapsedMS);
+
+            spriteBatch.DrawString(
+                GetSpriteFont(elapsedMS),
+                GetText(elapsedMS),
+                new Vector2(destRectangle.Left, destRectangle.Top),
+                color,
+                rotation,
+                origin,
+                scale,
+                spriteEffects,
+                layerDepth
+            );
+        }
+    }
+}
