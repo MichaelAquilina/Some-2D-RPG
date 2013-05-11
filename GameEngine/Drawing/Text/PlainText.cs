f6d8c2cd7bd470f87546c3dad39af72d921d4f57
KillaW0lf04 <michaelaquilina@gmail.com>
2013-05-11 20:47:41 +0200

    Added PlainText IGameDrawable to Text namespace.

---
+++ GameEngine/Drawing/Text/PlainText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameEngine.Drawing.Text
+{
+    public class PlainText : TextDrawable
+    {
+        public string Text { get; set; }
+
+        public SpriteFont SpriteFont { get; set; }
+
+        public PlainText()
+        {
+        }
+
+        public PlainText(SpriteFont spriteFont, string text)
+        {
+            Construct(spriteFont, text);
+        }
+
+        void Construct(SpriteFont spriteFont, string text)
+        {
+            this.Text = text;
+            this.SpriteFont = spriteFont;
+        }
+
+        public override int GetWidth(double elapsedMS)
+        {
+            return (int)Math.Ceiling(SpriteFont.MeasureString(Text).X);
+        }
+
+        public override int GetHeight(double elapsedMS)
+        {
+            return (int) Math.Ceiling(SpriteFont.MeasureString(Text).Y);
+        }
+
+        public override SpriteFont GetSpriteFont(double elapsedMS)
+        {
+            return SpriteFont;
+        }
+
+        public override string GetText(double elapsedMS)
+        {
+            return Text;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("PlainText: Text={0}, SpriteFont={1}", Text, SpriteFont);
+        }
+    }
+}
--- GameEngine/Drawing/Text/RegionText.cs
+++ GameEngine/Drawing/Text/RegionText.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GameEngine.Drawing.Text
 {
