8aa184084d3681346c627c4065e655b1d7846e23
KillaW0lf04 <michaelaquilina@gmail.com>
2013-05-23 12:39:23 +0200

    Added inside, victoria and kicthen tileset - along with a test map.

---
+++ Some2DRPG/Some2DRPGContent/Maps/Tilesets/kitchen.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kitchen" tilewidth="32" tileheight="32">
+ <properties>
+  <property name="Content" value="LPC\Terrain\kitchen"/>
+ </properties>
+ <image source="../../LPC/Terrain/kitchen.png" width="64" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Some2DRPG/Some2DRPGContent/Maps/Tilesets/victoria.tsx
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="victoria" tilewidth="32" tileheight="32">
+ <properties>
+  <property name="Content" value="LPC\Terrain\Victoria"/>
+ </properties>
+ <image source="../../LPC/Terrain/victoria.png" width="352" height="160"/>
+ <tile id="0">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Impassable" value=""/>
+  </properties>
+ </tile>
+</tileset>
