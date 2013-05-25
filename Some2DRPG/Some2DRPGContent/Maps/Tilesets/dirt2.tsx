ac8c450bb0a8d0a0f52fc12fe1a84bb225624609
KillaW0lf04 <michaelaquilina@gmail.com>
2013-05-25 12:35:13 +0200

    Updated sandbox map.

--- Some2DRPG/Some2DRPGContent/Maps/Tilesets/dirt2.tsx
+++ Some2DRPG/Some2DRPGContent/Maps/Tilesets/dirt2.tsx
@@ -4,4 +4,20 @@
   <property name="Content" value="LPC/Terrain/dirt2"/>
  </properties>
  <image source="../../LPC/Terrain/dirt2.png" width="96" height="192"/>
+ <terraintypes>
+  <terrain name="Dirt2" tile="-1"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,"/>
+ <tile id="2" terrain="0,0,,0"/>
+ <tile id="4" terrain="0,,0,0"/>
+ <tile id="5" terrain=",0,0,0"/>
+ <tile id="6" terrain=",,,0"/>
+ <tile id="7" terrain=",,0,0"/>
+ <tile id="8" terrain=",,0,"/>
+ <tile id="9" terrain=",0,,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,,0,"/>
+ <tile id="12" terrain=",0,,"/>
+ <tile id="13" terrain="0,0,,"/>
+ <tile id="14" terrain="0,,,"/>
 </tileset>
