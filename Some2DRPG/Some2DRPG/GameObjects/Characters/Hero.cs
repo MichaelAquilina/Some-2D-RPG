089acb08ae857bb20eb0ccdb3f962196abfdc835
KillaW0lf04 <michaelaquilina@gmail.com>
2013-06-02 17:46:57 +0200

    Added Mace weapon.

--- Some2DRPG/Some2DRPG/GameObjects/Characters/Hero.cs
+++ Some2DRPG/Some2DRPG/GameObjects/Characters/Hero.cs
@@ -53,7 +53,7 @@ namespace Some2DRPG.GameObjects.Characters
             QuickEquip("PlateGloves");
             QuickEquip("PlateChest");
             QuickEquip("PlateBoots");
-            QuickEquip("Rapier");
+            QuickEquip("Mace");
         }
 
         public override void PostCreate(GameTime gameTime, TeeEngine engine)
